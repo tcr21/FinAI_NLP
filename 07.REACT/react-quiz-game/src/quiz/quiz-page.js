c6a058a280cac56b4590399355434fbc58083fe9
tcr21 <108463412+tcr21@users.noreply.github.com>
2022-06-30 19:10:08 +0100

    Start API error handling

--- 07.REACT/react-quiz-game/src/quiz/quiz-page.js
+++ 07.REACT/react-quiz-game/src/quiz/quiz-page.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import Game from "./game";
+import LoadingSpinner from "../common/loading-spinner";
+import ErrorMessage from "../common/error-message";
 
 function QuizPage() {
-  return (
-    <main>
-      <Game />
-    </main>
-  );
+  const [quizFetch, setQuizFetch] = useState({
+    isLoading: true,
+    errorMessage: "",
+    data: null,
+  });
+
+  const { isLoading, errorMessage, data } = quizFetch;
+
+  let contents;
+  if (quizFetch.isLoading) contents = <LoadingSpinner />;
+  else if (quizFetch.errorMessage)
+    contents = <ErrorMessage>{errorMessage}</ErrorMessage>;
+  else contents = <Game />;
+
+  return <main>{contents}</main>;
 }
 
 export default QuizPage;
