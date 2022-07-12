47649d927d8b8231b31a836a35cdd57568b303ec
tcr21 <108463412+tcr21@users.noreply.github.com>
2022-07-12 16:45:40 +0100

    Classify input data onto clusters and visualise specific data line

--- NLP_modelling/high-level-classification.py
+++ NLP_modelling/high-level-classification.py
@@ -104,17 +104,19 @@ def get_similar_words(list_words, top_number, nlp_model):
 # Create dictionary {category:[keywords]}
 clusters_dict = {}
 clusters_dict["Route 1: Learning"] = get_similar_words(['learn','understand','skills','education'], 
-                  top_number=30, nlp_model=glove_model)
+                  top_number=10, nlp_model=glove_model)
 clusters_dict["Route 2: Personal finance"] = get_similar_words(['loan','need','money','family', 'debt', 'help', 'budget', 'household']
-                  , top_number=30, nlp_model=glove_model)
+                  , top_number=10, nlp_model=glove_model)
 clusters_dict["Route 3: Emergency"] = get_similar_words(['attack','threat','danger', 'death'], 
-                   top_number=30, nlp_model=glove_model)
+                   top_number=10, nlp_model=glove_model)
 # Print some similar words
 # for k,v in clusters_dict.items():
-#     print(k, ": ", v[0:30], "...", len(v))
+#     print(k, ": ", v[:], "...", len(v))
+
+
 
 # VISUALISE CLUSTERS===============================================================
-## word embedding
+# word embedding
 all_words = [word for v in clusters_dict.values() for word in v]
 X = glove_model[all_words]
         
@@ -122,14 +124,19 @@ X = glove_model[all_words]
 pca = manifold.TSNE(perplexity=40, n_components=2, init='pca')
 X = pca.fit_transform(X)
 
-## create dtf
+print("SHAPE X ===FOR TEST=====")
+print(X.shape)
+print(X)
+print("CLUSTER DICS===== FOR TEST=====")
+print(clusters_dict)
+
+# create dtf
 dtf = pd.DataFrame()
 for k,v in clusters_dict.items():
     size = len(dtf) + len(v)
     dtf_group = pd.DataFrame(X[len(dtf):size], columns=["x","y"], 
                              index=v)
     dtf_group["cluster"] = k
-    # TO DO: use pd.concat instead somehow (append will be deprecated in future frame version)
     dtf = pd.concat([dtf, dtf_group])
         
 ## plot
@@ -148,7 +155,7 @@ for i in range(len(dtf)):
 # plt.show()
 
 
-# # MAP DATA (USER ANSWERS) ON CLUSTERS==============================================
+# MAP DATA (USER ANSWERS) ON CLUSTERS==============================================
 
 # Tensorflow version - don't use
 # // tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
@@ -196,25 +203,224 @@ input_text_mean_vecs_list = [embed_text_with_bert(text, bert_tokenizer, bert_mod
 # create the feature matrix (n news x 768)
 X = np.array(input_text_mean_vecs_list)
 
+print("INPUT DATA==================")
+print(df["1. What is your primary concern when it comes to finance?_clean"])
+# print(input_text_mean_vecs_list)
+# print("INPUT DATA FEATURE MATRIX=======")
+# print(X.shape) # Returned (25, 768) due to 25 data lines
 # print(X)
 
-# TO DO: v is causing error below: TypeError: TextEncodeInput must be Union[TextInputSequence, Tuple[InputSequence, InputSequence]]
 # Apply to clusters 
-clusters_mean_vecs_dict = {k: embed_text_with_bert(v, bert_tokenizer, bert_model).mean(0) for k,v
-         in clusters_dict.items()}
+# clusters_mean_vecs_dict = {k: embed_text_with_bert(v, bert_tokenizer, bert_model).mean(0) for k,v
+#          in clusters_dict.items()}
+clusters_mean_vecs_list = [embed_text_with_bert(text, bert_tokenizer, bert_model).mean(0)
+                for text in dtf]
 
-print(clusters_mean_vecs_dict)
+# create feature matrix
+Y = np.array(clusters_mean_vecs_list)
+
+# create clusters dict mean
+clusters_mean_vecs_dict = {}
+clusters_mean_vecs_dict["Route 1: Learning"] = Y[0]
+clusters_mean_vecs_dict["Route 2: Personal finance"] = Y[1]
+clusters_mean_vecs_dict["Route 3: Emergency"] = Y[2]
+
+# print("CLUSTER DATAFRAME======")
+# print(dtf)
+# print("CLUSTER MEAN VECTOR LIST======")
+# print(clusters_mean_vecs_list)
+# print("CLUSTER FEATURE MATRIX======")
+# print(Y.shape) # Returned (3, 768) due to 3 clusters 
+# print(Y)
+# print("CLUSTER MEAN VECTOR DICT======")
+# print(clusters_mean_vecs_dict)
 
 
+# MODEL ALGORITHM==============================================
 
+# compute cosine similarities
+similarities = np.array(
+            [metrics.pairwise.cosine_similarity(X, Y)])
+            # .T.tolist()[0] 
+            #  for y in clusters_mean_vecs_dict.values()] # TO DO. Replace y with category?
+            # ).T
+print("SIMILARITIES SHAPE=====")
+similarities = similarities.reshape(25, 3)
+print(similarities.shape)
+print(similarities)
+# adjust and rescale
+labels = list(clusters_mean_vecs_dict.keys()) 
+for i in range(len(similarities)):
+    # assign randomly if there is no similarity
+    if sum(similarities[i]) == 0:
+       similarities[i] = [0]*len(labels)
+       similarities[i][np.random.choice(range(len(labels)))] = 1
+    # rescale so they sum = 1
+    similarities[i] = similarities[i] / sum(similarities[i])
 
-clusters_dict = {}
-clusters_dict["Route 1: Learning"] = get_similar_words(['learn','understand','skills','education'], 
-                  top_number=30, nlp_model=glove_model)
-clusters_dict["Route 2: Personal finance"] = get_similar_words(['loan','need','money','family', 'debt', 'help', 'budget', 'household']
-                  , top_number=30, nlp_model=glove_model)
-clusters_dict["Route 3: Emergency"] = get_similar_words(['attack','threat','danger', 'death'], 
-                   top_number=30, nlp_model=glove_model)
-# Print some similar words
+# classify the label with highest similarity score
+predicted_prob = similarities
+predicted = [labels[np.argmax(pred)] for pred in predicted_prob]
+
+print("PREDICTED LABELS========")
+print(predicted) 
+
+#TO DO: Output seems odd/ should not be this bad : has data been disordered somehow? Note nothing was categorised as route 1
+
+# EVALUATE=======================================================
+
+y_test = df["category"].values
+classes = np.unique(y_test)
+y_test_array = pd.get_dummies(y_test, drop_first=False).values
+
+# Accuracy, Precision, Recall
+accuracy = metrics.accuracy_score(y_test, predicted)
+auc = metrics.roc_auc_score(y_test, predicted_prob, 
+                            multi_class="ovr")
+print("Accuracy:",  round(accuracy,2))
+print("Auc:", round(auc,2))
+print("Detail:")
+print(metrics.classification_report(y_test, predicted))
+
+# Plot confusion matrix
+cm = metrics.confusion_matrix(y_test, predicted)
+fig, ax = plt.subplots()
+sns.heatmap(cm, annot=True, fmt='d', ax=ax, cmap=plt.cm.Blues, 
+            cbar=False)
+ax.set(xlabel="Pred", ylabel="True", xticklabels=classes, 
+       yticklabels=classes, title="Confusion matrix")
+plt.yticks(rotation=0)
+fig, ax = plt.subplots(nrows=1, ncols=2)
+
+# Plot roc
+for i in range(len(classes)):
+    fpr, tpr, thresholds = metrics.roc_curve(y_test_array[:,i],  
+                           predicted_prob[:,i])
+    ax[0].plot(fpr, tpr, lw=3, 
+              label='{0} (area={1:0.2f})'.format(classes[i], 
+                              metrics.auc(fpr, tpr))
+               )
+ax[0].plot([0,1], [0,1], color='navy', lw=3, linestyle='--')
+ax[0].set(xlim=[-0.05,1.0], ylim=[0.0,1.05], 
+          xlabel='False Positive Rate', 
+          ylabel="True Positive Rate (Recall)", 
+          title="Receiver operating characteristic")
+ax[0].legend(loc="lower right")
+ax[0].grid(True)
+    
+# Plot precision-recall curve
+for i in range(len(classes)):
+    precision, recall, thresholds = metrics.precision_recall_curve(
+                 y_test_array[:,i], predicted_prob[:,i])
+    ax[1].plot(recall, precision, lw=3, 
+               label='{0} (area={1:0.2f})'.format(classes[i], 
+                                  metrics.auc(recall, precision))
+              )
+ax[1].set(xlim=[0.0,1.05], ylim=[0.0,1.05], xlabel='Recall', 
+          ylabel="Precision", title="Precision-Recall curve")
+ax[1].legend(loc="best")
+ax[1].grid(True)
+plt.show()
+
+# UNDERSTAND EVALUATION=========================================
+
+i = 7
+txt_instance = df["1. What is your primary concern when it comes to finance?_clean"].iloc[i]
+print("True:", y_test[i], "--> Pred:", predicted[i], " | Similarity:", round(np.max(predicted_prob[i]),2))
+print(txt_instance)
+
+## create embedding Matrix
+# Same issue as before so used data frame
+# y = np.concatenate([embed_text_with_bert(v, bert_tokenizer, bert_model) for v in 
+#                     clusters_dict.values()])
+
+print(dtf.index)
+
+y = np.concatenate([embed_text_with_bert(text, bert_tokenizer, bert_model)
+                for text in dtf.index])
+
+print("y shape=====")
+print(y.shape) # (46, 768)
+print("y ARRAY PRINT==========")
+print(y)
+
+X = embed_text_with_bert(txt_instance, bert_tokenizer,
+                   bert_model).mean(0).reshape(1,-1)
+
+print("Printing X shape")
+print(X.shape) # (1, 768)
+print("Printing X======")
+print(X)
+
+M = np.concatenate([y,X])
+
+# pca
+pca = manifold.TSNE(perplexity=40, n_components=2, init='pca')
+M = pca.fit_transform(M)
+y, X = M[:len(y)], M[len(y):]
+
+print("M POST PCA")
+print(M)
+print("y POST PCA")
+print(y)
+print("X POST PCA")
+print(X)
+
+# FIXING
+# create dtf clusters
+dtf2 = pd.DataFrame()
+for k,v in clusters_dict.items():
+    size = len(dtf2) + len(v)
+    # ERROR: Shape of passed values is (4, 2), indices imply (14, 2)
+    dtf_group2 = pd.DataFrame(y[len(dtf2):size], columns=["x","y"],index=v)
+    dtf_group2["cluster"] = k
+    dtf2 = pd.concat([dtf2, dtf_group2])
+
+print("Printing dtf2=====")
+print(dtf2)
+
+# # FOR REF: create dtf
+# dtf = pd.DataFrame()
 # for k,v in clusters_dict.items():
-#     print(k, ": ", v[0:30], "...", len(v))
+#     size = len(dtf) + len(v)
+#     dtf_group = pd.DataFrame(X[len(dtf):size], columns=["x","y"], 
+#                              index=v)
+#     dtf_group["cluster"] = k
+#     dtf = pd.concat([dtf, dtf_group])
+
+
+## plot clusters
+fig, ax = plt.subplots()
+sns.scatterplot(data=dtf2, x="x", y="y", hue="cluster", ax=ax)
+ax.legend().texts[0].set_text(None)
+ax.set(xlabel=None, ylabel=None, xticks=[], xticklabels=[], 
+       yticks=[], yticklabels=[])
+for i in range(len(dtf)):
+    ax.annotate(dtf2.index[i], 
+               xy=(dtf2["x"].iloc[i],dtf2["y"].iloc[i]), 
+               xytext=(5,2), textcoords='offset points', 
+               ha='right', va='bottom')
+
+## add txt_instance
+ax.scatter(x=X[0][0], y=X[0][1], c="red", linewidth=10)
+ax.annotate("x", xy=(X[0][0],X[0][1]), 
+ha='center', va='center', fontsize=25)
+
+## calculate similarity
+sim_matrix = metrics.pairwise.cosine_similarity(X, y)
+
+## add top similarity
+for row in range(sim_matrix.shape[0]):
+    ### sorted {keyword:score}
+    dic_sim = {n:sim_matrix[row][n] for n in 
+               range(sim_matrix.shape[1])}
+    dic_sim = {k:v for k,v in sorted(dic_sim.items(), 
+                key=lambda item:item[1], reverse=True)}
+    ### plot lines
+    for k in dict(list(dic_sim.items())[0:5]).keys():
+        p1 = [X[row][0], X[row][1]]
+        p2 = [y[k][0], y[k][1]]
+        ax.plot([p1[0],p2[0]], [p1[1],p2[1]], c="red", alpha=0.5)
+plt.show()
+
+
