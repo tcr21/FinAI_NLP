cf20c481f2345820b8cb8e288b72baa477113ef8
tcr21 <108463412+tcr21@users.noreply.github.com>
2022-07-28 20:08:17 +0100

    MFI list page finished

---
+++ frontend/src/data/hooks/use-mfi-list.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function useMfiList() {
+    const [mfiListState, setMfiListState] = useState({
+      status: "loading",
+      results: null,
+      error: null,
+    });
+   
+    useEffect(() => {
+      async function callServerMfi() {
+        setMfiListState({
+          status: "loading",
+          results: null,
+          error: null,
+        });
+        
+        try {
+            axios
+                .post("http://127.0.0.1:5000/mfi")
+                .then((res) => {
+                console.log("Receiving server mfi output:", res);
+                setMfiListState({
+                    status: "success",
+                    results: res.data.data,
+                    error: null,
+                    });
+                })
+                .catch((err) => console.error(err));
+        } catch (error) {
+          console.error(error);
+          setMfiListState({ status: "error", data: null, error });
+        }
+      }
+      callServerMfi();
+    },[]); // Should this be empty?
+
+    const { status, results, error } = mfiListState;
+  
+    return {
+      status,
+      results,
+      error,
+    };
+  }
+  
+  export default useMfiList;
--- frontend/src/personal-finance/mfis-page.js
+++ frontend/src/personal-finance/mfis-page.js
@@ -1,89 +1,75 @@
-import axios from "axios";
-import { useState } from "react";
+// import axios from "axios";
+// import { useState } from "react";
+
+import ErrorMessage from "../common/error-message";
+import LoadingSpinner from "../common/loading-spinner";
+import useMfiList from "../data/hooks/use-mfi-list";
 
 function MfisPage() {
-  // VARIABLES AND FUNCTIONS------------------------
-  const [isLoading, setLoading] = useState(null);
-  const [mfiListResult, setMfiListResult] = useState(null);
-
-  const callServerMfi = () => {
-    console.log("In call server mfi function..."); 
-    setLoading(true);
-    axios
-      .post("http://127.0.0.1:5000/mfi")
-      .then((res) => {
-        console.log("Receiving server mfi output:", res);
-        setMfiListResult(res);
-        setLoading(false);
-      })
-      .catch((err) => console.error(err));
-  };
-
-  console.log("Mfi list result", mfiListResult);
-  console.log("Is loading", isLoading);
-
-  // BODY / CALLS TO FUNCTIONS----------------------------------------------
-
-  let mfiListData; 
-  if (mfiListResult !== null){
-    mfiListData = mfiListResult.data.data;  
-  }
-
-  console.log("Mfi list data:", mfiListData); 
-
-  // CONTENTS-----------------------------------------
-  let contents;
-
-  if (mfiListResult !== null){
-  contents = (<>
   
-  <div className="flex flex-col text-center w-full mb-0 py-10">
-          <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
-            LICENSED MICROFINANCE INSTITUTIONS
-          </h2>
-          <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">
-            Access a list of Sri Lanka's licensed MFIs
-          </h1>
-        </div>
+  let contents;
+  const mfiListInfo = useMfiList(); 
+  console.log("mfiListInfo", mfiListInfo); 
 
-        <button onClick={() => callServerMfi()}>Call server MFIs</button>
-  <div class="lg:w-1/1 w-full mx-auto overflow-auto">
-  <table class="table-auto w-full text-left whitespace-no-wrap">
-        <thead>
-          <tr>
-            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">#</th>
-            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Name</th>
-            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Address</th>
-            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Tel</th>
-            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Email</th>
-            <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">Website</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            mfiListData.map((value, key) => {
-              return (
-                <tr key={key}>
-                  <td class="border-t-2 border-gray-200 px-4 py-3 text-xs">{value.No}</td>
-                  <td class="border-t-2 border-gray-200 px-4 py-3 text-xs">{value.Name}</td>
-                  <td class="border-t-2 border-gray-200 px-4 py-3 text-xs">{value.Address}</td>
-                  <td class="border-t-2 border-gray-200 px-4 py-3 text-xs">{value.Tel}</td>
-                  <td class="border-t-2 border-gray-200 px-4 py-3 text-xs">{value.Email}</td>
-                  <td class="border-t-2 border-gray-200 px-4 py-3 text-xs"><a href={value.Website} target="_blank" rel="noreferrer"><button className="inline-flex items-center bg-indigo-500 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-xs mt-4 md:mt-0">Link to website</button></a></td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
-      </div>
-      <div className="text-gray-900 text-sm italic mb-5 py-2 text-center">
-            Source: Central Bank of Sri Lanka website
-          </div>
-  </>);} else {
+  if (mfiListInfo.status === "loading"){
+    contents = <LoadingSpinner />;
+  }
+  if (mfiListInfo.status === "error") {
+    contents = (
+      <ErrorMessage>Something went wrong. Please try again.</ErrorMessage>
+    );
+  }
+  // If don't have this, will try to render the below on null results before they've been set
+  if (mfiListInfo.results === null || mfiListInfo.results === undefined){
+    contents = <></>;
+  } 
+  else {
     contents = (<>
-      <button onClick={() => callServerMfi()}>Call server MFIs</button>
-      <>MFIs</><>No list yet</></>); 
+  
+      <div className="flex flex-col text-center w-full mb-0 py-10">
+              <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+                LICENSED MICROFINANCE INSTITUTIONS
+              </h2>
+              <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">
+                Access a list of Sri Lanka's licensed MFIs
+              </h1>
+            </div>
+    
+            
+      <div className="lg:w-1/1 w-full mx-auto overflow-auto">
+      <table className="table-auto w-full text-left whitespace-no-wrap">
+            <thead>
+              <tr>
+                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">#</th>
+                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Name</th>
+                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Address</th>
+                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Tel</th>
+                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Email</th>
+                <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">Website</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                mfiListInfo.results.map((value, key) => {
+                  return (
+                    <tr key={key}>
+                      <td className="border-t-2 border-gray-200 px-4 py-3 text-xs">{value.No}</td>
+                      <td className="border-t-2 border-gray-200 px-4 py-3 text-xs">{value.Name}</td>
+                      <td className="border-t-2 border-gray-200 px-4 py-3 text-xs">{value.Address}</td>
+                      <td className="border-t-2 border-gray-200 px-4 py-3 text-xs">{value.Tel}</td>
+                      <td className="border-t-2 border-gray-200 px-4 py-3 text-xs">{value.Email}</td>
+                      <td className="border-t-2 border-gray-200 px-4 py-3 text-xs"><a href={value.Website} target="_blank" rel="noreferrer"><button className="inline-flex items-center bg-indigo-500 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-xs mt-4 md:mt-0">Link to website</button></a></td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+          </div>
+          <div className="text-gray-900 text-sm italic mt-2 mb-5 py-2 text-center">
+                Source: Central Bank of Sri Lanka
+              </div>
+      </>);
   }
 
   return contents;
